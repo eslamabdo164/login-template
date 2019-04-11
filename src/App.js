9e26880e4af35f1bc1ca5d7a7aff9d56619cf1ae
eslamabdo164 <eslam.abdoweb@hotmail.com>
2019-04-12 01:55:02 +0200

    added signout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    route : ''
+  }
   render() {
     return (
       <div className="App">
         <p>Hello From inside the Website</p>
+        <a>Sign out</a>
       </div>
     );
   }
